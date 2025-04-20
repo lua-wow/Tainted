978458d5a8dfbcc182db6fd9d07222555a224a46
pedrozc90 <pedrozc90@gmail.com>
2025-04-20 15:34:15 -0300

    update dungeon portal spells for war within season 2

--- modules/miscellaneous/dungeon_portals.lua
+++ modules/miscellaneous/dungeon_portals.lua
@@ -8,86 +8,107 @@ if not C.miscellaneous.mythic.autokeystone then return end
 
 local portals = {
     -- Cataclysm
-    [438] = { spellID = 410080 }, -- The Vortex Pinnacle
-    [456] = { spellID = 424142 }, -- Throne of the Tides
-    [507] = { spellID = 445424 }, -- Grim Batol
+    [438] = { spells = { default = 410080 }, mapID = 657 }, -- The Vortex Pinnacle
+    [456] = { spells = { default = 424142 }, mapID = 643 }, -- Throne of the Tides
+    [507] = { spells = { default = 445424 }, mapID = 670 }, -- Grim Batol
+    
     -- Mists of Pandaria
-    [2] = { spellID = 131204 }, -- Temple of the Jade Serpent
-    [56] = { spellID = 131205 }, -- Stormstout Brewery
-    [57] = { spellID = 131225 }, -- Gate of the Setting Sun
-    [58] = { spellID = 131206 }, -- Shado-Pan Monastery
-    [59] = { spellID = 131228 }, -- Siege of Niuzao Temple
-    [60] = { spellID = 131222 }, -- Mogu'shan Palace
-    [76] = { spellID = 131232 }, -- Scholomance
-    [77] = { spellID = 131231 }, -- Scarlet Halls
-    [78] = { spellID = 131229 }, -- Scarlet Monastery
+    [2]  = { spells = { default = 131204 }, mapID = 960 }, -- Temple of the Jade Serpent
+    [56] = { spells = { default = 131205 }, mapID = 961 }, -- Stormstout Brewery
+    [57] = { spells = { default = 131225 }, mapID = 962 }, -- Gate of the Setting Sun
+    [58] = { spells = { default = 131206 }, mapID = 959 }, -- Shado-Pan Monastery
+    [59] = { spells = { default = 131228 }, mapID = 1011 }, -- Siege of Niuzao Temple
+    [60] = { spells = { default = 131222 }, mapID = 994 }, -- Mogu'shan Palace
+    [76] = { spells = { default = 131232 }, mapID = 1007 }, -- Scholomance
+    [77] = { spells = { default = 131231 }, mapID = 1001 }, -- Scarlet Halls
+    [78] = { spells = { default = 131229 }, mapID = 1004 }, -- Scarlet Monastery
+    
     -- WoD
-    [161] = nil, -- Skyreach
-    [163] = nil, -- Bloodmaul Slag Mines
-    [164] = nil, -- Archindoun
-    [165] = { spellID = 159899 }, -- Shadowmoon Burial Grounds
-    [166] = nil, -- Grimmrail Depot
-    [167] = nil, -- Upper Blackrock Spire
-    [168] = { spellID = 159901 }, -- The Everbloom
-    [169] = nil, -- Iron Docks
+    [161] = { spells = { default = 159898 }, mapID = 1209 }, -- Skyreach (Path of the Skies)
+    [163] = { spells = { default = 159895 }, mapID = 1175 }, -- Bloodmaul Slag Mines (Path of the Bloodmaul)
+    [164] = { spells = { default = 159897 }, mapID = 1182 }, -- Archindoun
+    [165] = { spells = { default = 159899 }, mapID = 1176 }, -- Shadowmoon Burial Grounds
+    [166] = { spells = { default = 159900 }, mapID = 1208 }, -- Grimmrail Depot (Path of the Dark Rail)
+    [167] = { spells = { default = 159902 }, mapID = 1358 }, -- Upper Blackrock Spire (Path of the Burning Mountain)
+    [168] = { spells = { default = 159901 }, mapID = 1279 }, -- The Everbloom
+    [169] = { spells = { default = 159896 }, mapID = 1195 }, -- Iron Docks (Path of the Iron Prow)
+    
     -- Legion
-    [197] = nil, -- Eye of Azshara
-    [198] = { spellID = 424163 }, -- Darkheart Thicket
-    [199] = { spellID = 424153 }, -- Black Rook Hold
-    [200] = { spellID = 393764 }, -- Halls of Valor
-    [206] = { spellID = 410078 }, -- Neltharion's Lair
-    [207] = nil, -- Vault of the Wardens
-    [208] = nil, -- Maw of Souls
-    [209] = nil, -- The Arcway
-    [210] = { spellID = 393766 }, -- Court of Stars
-    [227] = nil, -- Return of Karazhan: Lower
-    [233] = nil, -- Catheral of Eternal Night
-    [234] = nil, -- Return of Karazhan: Upper
-    [239] = nil, -- Seat of the Triumvirate
+    [197] = { spells = nil, mapID = 1456}, -- Eye of Azshara
+    [198] = { spells = { default = 424163 }, mapID = 1466 }, -- Darkheart Thicket
+    [199] = { spells = { default = 424153 }, mapID = 1501 }, -- Black Rook Hold
+    [200] = { spells = { default = 393764 }, mapID = 1477 }, -- Halls of Valor
+    [206] = { spells = { default = 410078 }, mapID = 1458 }, -- Neltharion's Lair
+    [207] = { spells = nil, mapID = 1493 }, -- Vault of the Wardens
+    [208] = { spells = nil, mapID = 1492 }, -- Maw of Souls
+    [209] = { spells = nil, mapID = 1516 }, -- The Arcway
+    [210] = { spells = { default = 393766 }, mapID = 1571 }, -- Court of Stars
+    [227] = { spells = { default = 373262 }, mapID = 1651 }, -- Return of Karazhan: Lower
+    [233] = { spells = nil, mapID = 1677 }, -- Catheral of Eternal Night
+    [234] = { spells = { default = 373262 }, mapID = 1651 }, -- Return of Karazhan: Upper
+    [239] = { spells = nil, mapID = 1753 }, -- Seat of the Triumvirate
+    
     -- Battle for Azeroth
-    [244] = { spellID = 424187 }, -- Atal'Dazar
-    [245] = { spellID = 410071 }, -- Freehold
-    [246] = nil, -- Tol Dagor
-    [247] = nil, -- The MOTHERLODE!!
-    [248] = { spellID = 424167 }, -- Waycrest Manor
-    [249] = nil, -- Kings' Rest
-    [250] = nil, -- Temple of Sethraliss
-    [251] = { spellID = 410074 }, -- The Underrot
-    [252] = nil, -- Temple of the Storm
-    [353] = { ["Alliance"] = 445418, ["Horde"] = 464256 }, -- Siege of Boralus
-    [369] = nil, -- Operation: Mechagon - Junkyard
-    [370] = nil, -- Operation: Mechagon - Workshop
+    [244] = { spells = { default = 424187 }, mapID = 1763 }, -- Atal'Dazar
+    [245] = { spells = { default = 410071 }, mapID = 1754 }, -- Freehold
+    [246] = { spells = nil, mapID = 1771 }, -- Tol Dagor
+    [247] = { spells = { Alliance = 467553, Horde = 467555 }, mapID = 1594 }, -- The MOTHERLODE!!
+    [248] = { spells = { default = 424167 }, mapID = 1862 }, -- Waycrest Manor
+    [249] = { spells = nil, mapID = 1762 }, -- Kings' Rest
+    [250] = { spells = nil, mapID = 1877 }, -- Temple of Sethraliss
+    [251] = { spells = { default = 410074 }, mapID = 1841 }, -- The Underrot
+    [252] = { spells = nil, mapID = 1864 }, -- Temple of the Storm
+    [353] = { spells = { Alliance = 445418, Horde = 464256 }, mapID = 1822 }, -- Siege of Boralus
+    [369] = { spells = { default = 373274 }, mapID = 2097 }, -- Operation: Mechagon - Junkyard
+    [370] = { spells = { default = 373274 }, mapID = 2097 }, -- Operation: Mechagon - Workshop
+    
     -- Shadowlands
-    [375] = { spellID = 354464 }, -- Mists of Tirna Scithe
-    [376] = { spellID = 354462 }, -- The Necrotic Wake
-    [377] = nil, -- De Other Side
-    [378] = nil, -- Halls of Atonement
-    [379] = nil, -- Plaguefall
-    [380] = nil, -- Sanguine Depths
-    [381] = { spellID = 354462 }, -- Spires of Ascensione
-    [382] = nil, -- Theather of Pain
-    [391] = nil, -- Tazavesh: Streets of Wonder
-    [392] = nil, -- Tazavesh: So'leash's Gambit
+    [375] = { spells = { default = 354464 }, mapID = 2290 }, -- Mists of Tirna Scithe (Path of the Misty Forest)
+    [376] = { spells = { default = 354462 }, mapID = 2286 }, -- The Necrotic Wake (Path of the Courageous)
+    [377] = { spells = { default = 354468 }, mapID = 2291 }, -- De Other Side (Path of the Scheming Loa)
+    [378] = { spells = { default = 354465 }, mapID = 2287 }, -- Halls of Atonement (Path of the Sinful Soul)
+    [379] = { spells = { default = 354463 }, mapID = 2289 }, -- Plaguefall (Path of the Plagued)
+    [380] = { spells = { default = 354469 }, mapID = 2284 }, -- Sanguine Depths (Path of the Stone Warden)
+    [381] = { spells = { default = 354466 }, mapID = 2285 }, -- Spires of Ascension (Path of the Ascendant)
+    [382] = { spells = { default = 354467 }, mapID = 2293 }, -- Theater of Pain (Path of the Undefeated)
+    [391] = { spells = { default = 367416 }, mapID = 2441 }, -- Tazavesh: Streets of Wonder (Path of the Streetwise Merchant)
+    [392] = { spells = { default = 367416 }, mapID = 2441 }, -- Tazavesh: So'leash's Gambit (Path of the Streetwise Merchant)
+    
+    -- Shadowlands Raids
+    ["Nathria"] = { spells = { default = 373190 }, mapID = 0 }, -- Castle Nathria (Path of the Sire)
+    ["Sanctum"] = { spells = { default = 373191 }, mapID = 0 }, -- Sanctum of Domination (Path of the Tormented Soul)
+    ["Sepulcher"] = { spells = { default = 373192 }, mapID = 0 }, -- Sepulcher of the First Ones (Path of the First Ones)
+    
     -- Dragonflight
-    [399] = { spellID = 393256 }, -- Ruby Life Pools
-    [400] = { spellID = 393262 }, -- The Nokhud Offensive
-    [401] = { spellID = 393279 }, -- The Azure Vault
-    [402] = { spellID = 393273 }, -- Algeth'ar Academy
-    [403] = { spellID = 393222 }, -- Uldaman: Legacy of Tyr
-    [404] = { spellID = 393276 }, -- Neltharus
-    [405] = { spellID = 393267 }, -- Bracknhide Hollow
-    [406] = { spellID = 393283 }, -- Halls of Infusion
-    [463] = { spellID = 424197 }, -- Dawn of the Infinite: Galakrond's Fall
-    [464] = { spellID = 424197 }, -- Dawn of the Infinite: Murozond's Rise
+    [399] = { spells = { default = 393256 }, mapID = 2521 }, -- Ruby Life Pools
+    [400] = { spells = { default = 393262 }, mapID = 2516 }, -- The Nokhud Offensive
+    [401] = { spells = { default = 393279 }, mapID = 2515 }, -- The Azure Vault
+    [402] = { spells = { default = 393273 }, mapID = 2526 }, -- Algeth'ar Academy
+    [403] = { spells = { default = 393222 }, mapID = 2551 }, -- Uldaman: Legacy of Tyr
+    [404] = { spells = { default = 393276 }, mapID = 2519 }, -- Neltharus
+    [405] = { spells = { default = 393267 }, mapID = 2520 }, -- Bracknhide Hollow
+    [406] = { spells = { default = 393283 }, mapID = 2527 }, -- Halls of Infusion
+    [463] = { spells = { default = 424197 }, mapID = 2579 }, -- Dawn of the Infinite: Galakrond's Fall
+    [464] = { spells = { default = 424197 }, mapID = 2579 }, -- Dawn of the Infinite: Murozond's Rise
+        
+    -- Dragonflight Raids
+    ["Vault"] = { spells = { default = 432254 }, mapID = 0 }, -- Vault of the Incarnates
+    ["Aberrus"] = { spells = { default = 432257 }, mapID = 0 }, -- Aberrus, the Shadowed Crucible
+    ["Amirdrassil"] = { spells = { default = 432258 }, mapID = 0 }, -- Amirdrassil, the Dream's Hope
+
     -- The War Within
-    [499] = nil, -- Priory of the Sacred Flame
-    [500] = nil, -- The Rookery
-    [501] = { spellID = 445269 }, -- The Stonevault
-    [502] = { spellID = 445416 }, -- City of Threads
-    [503] = { spellID = 445417 }, -- Ara-Kara, City of Echos
-    [504] = nil, -- Darkflame Cleft
-    [505] = { spellID = 445414 }, -- The Dawnbreaker
-    [506] = nil, -- Cinderbrew Meadery
+    [499] = { spells = { default = 445444 }, mapID = 2649 }, -- Priory of the Sacred Flame
+    [500] = { spells = { default = 445443 }, mapID = 2648 }, -- The Rookery
+    [501] = { spells = { default = 445269 }, mapID = 2652 }, -- The Stonevault
+    [502] = { spells = { default = 445416 }, mapID = 2669 }, -- City of Threads
+    [503] = { spells = { default = 445417 }, mapID = 2660 }, -- Ara-Kara, City of Echos
+    [504] = { spells = { default = 445441 }, mapID = 2651 }, -- Darkflame Cleft
+    [505] = { spells = { default = 445414 }, mapID = 2651 }, -- The Dawnbreaker
+    [506] = { spells = { default = 445440 }, mapID = 2661 }, -- Cinderbrew Meadery (Path of the Flaming Brewery)
+    [525] = { spells = { default = 1216786 }, mapID = 2773 }, -- Operation: Floodgate
+    
+    -- The War Within Raids
+    ["Undermine"] = { spells = { default = 1226482 }, mapID = 0 }, -- Liberation of Undermine
 }
 
 local button_proto = {}
@@ -153,18 +174,26 @@ do
         end
     end
 
+    function portal_proto:GetPortalSpell(mapID, faction)
+        local dungeon = portals[mapID]
+        return dungeon and dungeon.spells and (dungeon.spells[faction] or dungeon.spells.default) or nil
+    end
+
     function portal_proto:CreateButton(index, mapID)
         local fontObject = E.GetFont(C.miscellaneous.font)
 
         local faction, _ = UnitFactionGroup("player")
         local mapName, _, _, mapTexture, mapBackgroundTexture = C_ChallengeMode.GetMapUIInfo(mapID)
 
-        local portalData = portals[mapID]
-        local spellID = portalData.spellID or portalData[faction] or 8690
-        local spellName = C_Spell.GetSpellName(spellID)
+        local spellID = self:GetPortalSpell(mapID, faction)
+        if not spellID then
+            E:error("Dungeon " .. mapName .. " (" .. mapID .. ") is missing the spellID")
+            return nil
+        end
 
-        if spellID == 8690 then
-            E:error("Missing portal spell for " .. mapName .. " (" .. mapID .. ")")
+        local spellName = C_Spell.GetSpellName(spellID)
+        if not spellName then
+            E:error("Dungeon " .. mapName .. " (" .. mapID .. ") protal spell " .. spellID .. " do not exists")
         end
 
         -- Create button for each known spell
